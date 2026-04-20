34872debac69f7a4a722bb6fb318318489bbb61b
luisjimenezgarcia <luisjimenezgarcia2006@gmail.com>
2026-04-20 18:59:34 +0200

    Corregir función serverless

--- netlify/functions/app.py
+++ netlify/functions/app.py
@@ -5,7 +5,7 @@ import sys
 import os
 
 # Añadir la raíz del proyecto al path
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
 # Importar tu aplicación original
 from app import app as application
