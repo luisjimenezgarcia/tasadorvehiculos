02a5af850bc15969d534a2bcf11872d275cd1941
luisjimenezgarcia <luisjimenezgarcia2006@gmail.com>
2026-04-20 16:52:21 +0200

    Añadir función serverless

---
+++ netlify/functions/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from serverless_wsgi import handle_request
+
+import sys
+import os
+
+# Añadir la raíz del proyecto al path
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Importar tu aplicación original
+from app import app as application
+
+def handler(event, context):
+    return handle_request(application, event, context)
